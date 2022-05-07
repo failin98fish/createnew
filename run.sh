97aaf19be11f2363f2beb39a3da2af7c547b5ccf
failin98fish <56397393+failin98fish@users.noreply.github.com>
2022-05-07 14:33:31 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,10 +9,6 @@ echo "更改root密码"
 
 su root << EOF 
 857694840
-EOF
-
-
-
 
 a=`uname  -a`
  
@@ -40,6 +36,31 @@ elif [[ $a =~ $C ]];then
     echo $C
 elif [[ $a =~ $U ]];then
     echo $U
+    "开始安装docker"
+    sudo apt-get remove docker docker-engine docker.io containerd runc
+    sudo apt-get update
+    sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo \
+  	"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  	$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+     sudo apt-get update
+     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+     echo "finished"
 else
     echo $a
 fi
+
+
+
+
+EOF
+
+
+
+
+
